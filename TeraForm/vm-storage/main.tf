2627ea96dc78ee8f4e80f1cda52e80a03878db12
Rohit Raut <69861855+rohitpatil2137@users.noreply.github.com>
2024-08-14 14:16:43 +0530

    Update main.tf

--- TeraForm/vm-storage/main.tf
+++ TeraForm/vm-storage/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "rg"
+  name     = "az-rg"   #on azure portal, resource group will create as "az-rg"
   location = var.region
 }
 
 resource "azurerm_storage_account" "stg-account" {
-  name                     = "stgbhosdike"
+  name                     = "az-stg-account"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -12,13 +12,13 @@ resource "azurerm_storage_account" "stg-account" {
 }
 
 resource "azurerm_storage_container" "stg-ctr" {
-  name                  = "stg-ctr"
+  name                  = "az-stg-ctr"
   storage_account_name  = azurerm_storage_account.stg-account.name
   container_access_type = "private"
 }
 
 resource "azurerm_storage_blob" "example" {
-  name                   = "Hi.txt"
+  name                   = "az-Hi.txt"
   storage_account_name   = azurerm_storage_account.stg-account.name
   storage_container_name = azurerm_storage_container.stg-ctr.name
   type                   = "Block"
